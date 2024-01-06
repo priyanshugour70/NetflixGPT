0e40779d957ed50bbfe7131ce530dd881524608e
priyanshugour70 <priyanshugour1@gmail.com>
2024-01-06 22:28:51 +0530

    Setup Route, UI design of Header & Login then abstract of SingUp

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10">
+      <img
+        className="w-44 "
+        src="https://www.freepnglogos.com/uploads/netflix-logo-0.png"
+        alt=""
+      />
+    </div>
+  );
+};
 
-export default Header
+export default Header;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import Header from "./Header";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div>
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/c38a2d52-138e-48a3-ab68-36787ece46b3/eeb03fc9-99c6-438e-824d-32917ce55783/IN-en-20240101-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="Netlfix Background Img"
+        />
+      </div>
 
-export default Login
+      <form
+        action=""
+        className=" w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80"
+      >
+        <h1 className="font-bold text-3xl py-4">Sign In</h1>
+        <input
+          type="text"
+          placeholder="Email & Phone Number"
+          className="p-4 my-4 w-full bg-gray-600"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-gray-600"
+        />
+        <button className="p-4 my-6 bg-red-700 w-full rounded-lg">
+          Sign In
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
